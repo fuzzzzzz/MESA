95ef2c4426a9448c032c823b8a019e94c846f9e6
fuzzzzzz <fuzzzzzz@gmail.com>
2013-10-06 10:09:27 +0200

    commit default mesa config  batch.

--- config/Mesa_config.bat
+++ config/Mesa_config.bat
@@ -10,26 +10,25 @@ SET MESADIR=[ENTER-MESA-PATH]
 SETX MESADIR %MESADIR%
 echo MESADIR=%MESADIR%
 
-:: Set mesa Root Directory path on the disk.
-::Example:
-::SET MESADIR=C:\MESA
-SETX MESADIR %MESADIR%
-echo MESADIR=%MESADIR%
+
 
 :: WARNING ! 
 :: The " is only at the beginning of the path, and there is not quote at the end and no whitespace.
 :: Example : 
 ::SET STEAMDIR="C:\Program Files (x86)\Steam
+SET STEAMDIR="C:\Program Files (x86)\Steam
 SETX STEAMDIR %STEAMDIR%
 echo STEAMDIR=%STEAMDIR%
 
 :: WARNING !
 :: This is the username profile create on steam, and refer to the steam\steamapps\<USERNAME>\ directory to find the Binaries tools for source 2006, 2007 and 2009 tools.
+SET STEAMUSER =ENTER-STEAM-USER
 SETX STEAMUSER %STEAMUSER%
 echo STEAMUSER=%STEAMUSER%
 
 :: This is required to copy the MESA shelf into the proper place in maya user folders.
 :: Refer to the Suggested list of version of maya.
+SET MAYAVERSION=2012x64
 SETX MAYAVERSION %MAYAVERSION%
 echo MAYAVERSION=%MAYAVERSION%
 
