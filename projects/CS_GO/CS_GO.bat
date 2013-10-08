da857b91417d55a07bb359649e86ff5cb3237a17
fuzzzzzz <fuzzzzzz@gmail.com>
2013-10-08 16:50:30 +0200

    fix GS GO project batch file

--- projects/CS_GO/CS_GO.bat
+++ projects/CS_GO/CS_GO.bat
@@ -6,12 +6,12 @@ call "..\..\config\mesa_check_env.bat"
 
 echo "Set Bin directory path, where Studiomdl.exe and "vtex.exe" are located."
 echo ...
-SET SOURCE_BIN_PATH=""%STEAMDIR%\steamapps\common\Counter-Strike Globale Offensive\bin"
+SET SOURCE_BIN_PATH=""%STEAMDIR%\steamapps\common\Counter-Strike Global Offensive\bin"
 SETX SOURCE_BIN_PATH ""%SOURCE_BIN_PATH%"
 echo SOURCE_BIN_PATH=%SOURCE_BIN_PATH%
 
 echo Set the Game Project Directory, it must be the same path than VPROJECT.
-SET GAME_PROJECT=%STEAMDIR%\steamapps\common\Counter-Strike Globale Offensive\csgo
+SET GAME_PROJECT=%STEAMDIR%\steamapps\common\Counter-Strike Global Offensive\csgo
 SETX GAME_PROJECT "%GAME_PROJECT%"
 echo GAME_PROJECT=%GAME_PROJECT%
 
@@ -23,13 +23,13 @@ echo GAME_PROJECT_SRC=%GAME_PROJECT_SRC%
 
 echo Set the name of your Game project here
 echo ...
-SET GAME_PROJECT_NAME="Counter-Strike Globale Offensive"
+SET GAME_PROJECT_NAME="Counter-Strike Global Offensive"
 SETX GAME_PROJECT_NAME %GAME_PROJECT_NAME%
 echo GAME_PROJECT_NAME=%GAME_PROJECT_NAME%
 
 
 echo Set the VPROJECT Directory, it must be the same path than GAME_PROJECT.
 echo ...
-SET VPROJECT=%STEAMDIR%\steamapps\common\Counter-Strike Globale Offensive\csgo
+SET VPROJECT=%STEAMDIR%\steamapps\common\Counter-Strike Global Offensive\csgo
 SETX VPROJECT "%VPROJECT%"
 echo VPROJECT=%VPROJECT%
